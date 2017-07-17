9ae4be9744fccce0fbd94d19e0a1f91adcbe1dc2
marhass <marhass@live.unc.edu>
2017-07-16 22:01:35 -0400

    ?

---
+++ marhass-convert-documents/marhass-convert-docs.sh
@@ -0,0 +1,14 @@
+#!bin/bash/
+
+# Declare input argument as a variable
+INPUTFILE=file
+OUTPUTFILE=
+# Convert markdown to HTML
+pandoc -o file.html file.md
+# Convert markdown to DOCX
+pandoc -o file.
+# Convert markdown to ODT
+
+# Convert markdown to PDF
+
+# Make the command line read "Converted INPUT-FILENAME to HTML, DOCX, ODT, PDF
